6d5cd5ac9153013c7e3daec08e78e44dbdd8a1d5
Bhavana N B <93139973+bhavananb062020@users.noreply.github.com>
2024-09-20 00:09:06 +0530

    Update dbcon.php

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', "srv1496.hstgr.io");
-define('DB_USERNAME', "u730102058_trailer");
-define('DB_PASSWORD', "Isitok@2003");
-define('DB_DATABASE', "u730102058_trailer");
+define('DB_SERVER',"localhost");
+define('DB_USERNAME', "root");
+define('DB_PASSWORD', "");
+define('DB_DATABASE', "trailer");
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
